b6db7b48415601036553f483a473728f2db898ef
narawich2111 <85145428+narawich2111@users.noreply.github.com>
2022-10-06 16:34:16 +0700

    new color btn

--- TrialApp/WinFormsApp1/Form1.Designer.cs
+++ TrialApp/WinFormsApp1/Form1.Designer.cs
@@ -64,7 +64,7 @@ namespace WinFormsApp1
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.BackColor = System.Drawing.SystemColors.Desktop;
             this.button1.Location = new System.Drawing.Point(31, 91);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(538, 38);
